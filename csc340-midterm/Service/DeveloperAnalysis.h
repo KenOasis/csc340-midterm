8da23850be5fa0154b596f9f8ff9ae83fac8a399
KenOasis <jtan17@mail.sfsu.edu>
2020-10-30 21:25:18 -0700

    Service header added

---
+++ csc340-midterm/Service/DeveloperAnalysis.h
@@ -0,0 +1,35 @@
+//
+//  DeveloperAnalysis.h
+//  csc340-midterm
+//
+//  Created by Jinjian Tan on 10/30/20.
+//
+
+#ifndef DeveloperAnalysis_h
+#define DeveloperAnalysis_h
+class DeveloperAnalysis{
+public:
+    /*
+        This function addresses the question b) in the proposal
+     
+        1) get all the games
+        2) count the number of entry which "publisher" equal "developer"
+        3) count the percentage as (the above number/total number of games) * 100%
+        4) print the result
+     
+     */
+    void getPercentageDevByPub();
+    
+    /*
+        This function addresses the question c) in the proposal
+        parameter @developer is the developer to be analyzed
+        parameter @fieldname is the field be used to sort the game
+                             it could be sales or userscore
+        1) get all the games by given developer
+        2) sort the games by the given fields (sales or userscores)
+        3) print out top 10 games of the sorting result.
+     */
+    void getPopularGamesByDeveloper(string developer string fieldname)
+    
+}
+#endif /* DeveloperAnalysis_h */
---
+++ csc340-midterm/Service/GenreAnalysis.h
@@ -0,0 +1,56 @@
+//
+//  GenreAnalysis.h
+//  csc340-midterm
+//
+//  Created by Jinjian Tan on 10/30/20.
+//
+
+#ifndef GenreAnalysis_h
+#define GenreAnalysis_h
+class GenreAnalysis{
+public:
+    /*
+        This function addresses the question a) in the proposal
+        
+        steps (in general):
+        1) sort all the game base on the sales
+        2) set tokens to count how many games for a given genre
+           occurs in the first 50% of the sorted games.
+           for example, if there are 15 games of genre "platform"
+           in the sorted games, then the token of "platform" should
+           be 15.
+        3) count the total(global) sales of games for a given genre;
+            
+        4) List(print) the top 5 genres by tokens and print their total sales. (sort by tokens but not sales)
+         
+    */
+    void getPopularGenresBySales();
+    
+    /*
+        This function addresses the question d) in the proposal
+
+        parameter @fieldname is the field be used to sort the games, it could be developer, sales, userscores or critic scores.
+        parameter @platform is the platform to be researched
+        
+     steps (in general):
+        1) get all the games by the given platform
+        2) sort them by the given field.
+        3) set tokens to count how many games for a give genre occures in all the games.
+        4) List(print) the top 5 genres by tokens.
+       
+     */
+    void getPopularGenresByPlaform(string platform, string fieldname);
+    /*
+        This function addresses the question f) in the proposal
+     
+        parameter @genre is the genre to be analyzed
+     
+        1) get all the games by the given genre
+        2) set tokens to count how many games in each year (1984-2015) , for example, if genre "platform" appeared
+           15 times in 1984, then the token "1984" should be 15
+        3) list all the tokens and do a litter analysis on it
+           (increasing or decrease by year? floating? flat?)
+     */
+    void getGenereOverYear(string genre);
+
+#endif /* GenreAnalysis_h */
