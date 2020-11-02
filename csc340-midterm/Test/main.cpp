417a22e069058ceb5449ba1307f518b45339d69c
kami <kamisawekchim@gmail.com>
2020-11-01 19:09:40 -0800

    updated print menu to match proposal

--- csc340-midterm/Test/main.cpp
+++ csc340-midterm/Test/main.cpp
@@ -5,7 +5,8 @@
 //  Created by Kami Sawekchim on 10/30/20.
 //
 #include <iostream>
-
+#include "GenreAnalysis.h"
+#include "DeveloperAnalyis.h"
 
 using namespace std;
 
@@ -17,38 +18,43 @@ void PrintMenu() {
 
    do {
       cout << "What question would you like to ask?" << endl;
-      cout << "1 - Based on global/NA/EU/Japan/other video game sales and development, what are the ten most popular video game genres?" << endl;
-      cout << "2 - Based on global/NA/EU/Japan/other video game sales and development, what are the ten least popular video game genres?" << endl;
-      cout << "3 - What percentage of developers outsource their publishing?" << endl;
-      cout << "4 - How many developers use the same publisher?" << endl;
-      cout << "5 - For each specific platform, what are the most popular genres of games based on development, sales, user scores, and critic scores? " << endl;
-      cout << "6 - No Questions. Quit. " << endl;
+      cout << "A - Based on global/NA/EU/Japan/other video game sales and development, what are the ten most popular video game genres?" << endl;
+      cout << "B - What percentage of developers outsource their publishing?" << endl;
+      cout << "C - For a given developer, what are the most popular games they developed (by scores or by sales)" << endl;
+      cout << "D - For each specific platform, what are the most popular genres of games based on sales? " << endl;
+      cout << "E - For each specific platform, what are the most popular genres of games based on user scores? " << endl;
+      cout << "F -  How has the popularity of certain genres changed over time? Which ones have grown? Which ones have fallen in popularity?" << endl;
+      cout << "Q - No Questions. Quit. " << endl;
 
-      cout << "Choose a number: ";
+      cout << "Choose a letter: ";
       cin >> option;
 
       switch (option) {
-      case '1':
-         cout << "Based on global/NA/EU/Japan/other video game sales and development, what are the ten most popular video game genres?" << GetMostPopVG() << endl;
+      case 'A':
+         cout << "Based on global/NA/EU/Japan/other video game sales and development, what are the ten most popular video game genres?" << getPopularGenresBySales(vector<Game> games) << endl;
          break;
 
-      case '2':
-         cout << "Based on global/NA/EU/Japan/other video game sales and development, what are the ten least popular video game genres?" << GetLeastPopVG() << endl;
+      case 'B':
+         cout << " What percentage of developers outsource their publishing?" << getPercentageDevByPub() << endl;
          break;
 
-      case '3':
-         cout << "What percentage of developers outsource their publishing?" << GetDevOutsource() << endl; // please add correct get function
+      case 'C':
+         cout << "For a given developer, what are the most popular games they developed (by scores or by sales)" << getPopularGamesByDeveloper(string developer string fieldname)<< endl; 
          break;
 
-      case '4':
-         cout << "How many developers use the same publisher?" << GetSamePub() << endl; // please add correct get function
+      case 'D':
+         cout << "For each specific platform, what are the most popular genres of games based on sales? " << getPopularGenresByPlaform(string platform, string fieldname);
          break;
 
-      case '5':
-         cout << "For each specific platform, what are the most popular genres of games based on development, sales, user scores, and critic scores? " << GetMostBasedOn());
+      case 'E':
+         cout << "For each specific platform, what are the most popular genres of games based on user scores? " << getPopularGenresByPlaform(string platform, string fieldname);
          break;
 
-      case '6':        
+      case 'F':
+         cout << "For each specific platform, what are the most popular genres of games based on user scores? " << getGenereOverYear(string genre);
+         break;
+
+      case 'Q':        
          cout << "No Questions. Quit."  << endl;
          break;
 
@@ -56,7 +62,7 @@ void PrintMenu() {
          cout << "option unknown." << endl;
          break;
    }
-   }while (option != '6');
+   }while (option != 'Q');
 
 }
 
